b3fac5904e3c833825bcf3c3487ca027f4d6aee9
Tabbs18 <tabithamclaughlin500@yahoo.com>
2019-05-21 14:10:15 -0400

    ceated all files, finished the home html, and started the friends.js object array.

---
+++ app/friends.js
@@ -0,0 +1,71 @@
+let friends = [
+    {
+      "name":"Sand Man",
+      "photo":"",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          3
+        ] 
+    },
+    {
+      "name":"Iron Man",
+      "photo":"",
+      "scores":[
+          3,
+          2,
+          5,
+          1,
+          2
+        ]
+    },
+    {
+      "name":"Aqua Man",
+      "photo":"",
+      "scores":[
+          2,
+          3,
+          4,
+          4,
+          3
+        ]
+    },
+    {
+      "name":"Super Man",
+      "photo":"",
+      "scores":[
+          5,
+          5,
+          5,
+          5,
+          4
+        ]
+    },
+    {
+      "name":"Spider Man",
+      "photo":"",
+      "scores":[
+          2,
+          3,
+          4,
+          5,
+          1
+        ]
+    },
+    {
+      "name":"Bat Man",
+      "photo":"",
+      "scores":[
+          4,
+          2,
+          3,
+          1,
+          4
+        ]
+    }
+    ];
+    
+    module.exports = friends;
+    
