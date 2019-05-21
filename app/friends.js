4778501c6440bb90466a9c421e5d220c94f7f7df
Tabbs18 <tabithamclaughlin500@yahoo.com>
2019-05-21 14:29:29 -0400

    Finished friends.js, added in images and changed names

--- app/friends.js
+++ app/friends.js
@@ -1,7 +1,7 @@
 let friends = [
     {
       "name":"Sand Man",
-      "photo":"",
+      "photo":"https://en.wikipedia.org/wiki/Sandman_(Marvel_Comics)#/media/File:Tboltssandman.jpg",
       "scores":[
           5,
           1,
@@ -12,7 +12,7 @@ let friends = [
     },
     {
       "name":"Iron Man",
-      "photo":"",
+      "photo":"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjd7pKuna3iAhXKmuAKHQGrB4wQjRx6BAgBEAU&url=https%3A%2F%2Fwww.ign.com%2Fvideos%2F2019%2F04%2F02%2Firon-man-vr-hands-on-impressions&psig=AOvVaw34672nKP69cVJim9jjvQzD&ust=1558548904499543",
       "scores":[
           3,
           2,
@@ -22,8 +22,8 @@ let friends = [
         ]
     },
     {
-      "name":"Aqua Man",
-      "photo":"",
+      "name":"elvis presly",
+      "photo":"https://timedotcom.files.wordpress.com/2018/06/elvis-presley-documentary-the-king.jpg",
       "scores":[
           2,
           3,
@@ -33,8 +33,8 @@ let friends = [
         ]
     },
     {
-      "name":"Super Man",
-      "photo":"",
+      "name":"Luke Bryan",
+      "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fiscale.iheart.com%2Fcatalog%2Fartist%2F29083&imgrefurl=https%3A%2F%2Fwww.iheart.com%2Fartist%2Fluke-bryan-29083%2F&docid=wGthwaiCpGSsTM&tbnid=pn17YD9KxiQlcM%3A&vet=10ahUKEwjarLGKn63iAhWJhOAKHZp8BrIQMwh7KAIwAg..i&w=720&h=1080&bih=657&biw=1366&q=luke%20bryan&ved=0ahUKEwjarLGKn63iAhWJhOAKHZp8BrIQMwh7KAIwAg&iact=mrc&uact=8",
       "scores":[
           5,
           5,
@@ -44,8 +44,8 @@ let friends = [
         ]
     },
     {
-      "name":"Spider Man",
-      "photo":"",
+      "name":"whitney houston",
+      "photo":"https://www.rollingstone.com/wp-content/uploads/2018/07/shutterstock_4627232d.jpg?crop=900:600&width=440",
       "scores":[
           2,
           3,
@@ -56,7 +56,7 @@ let friends = [
     },
     {
       "name":"Bat Man",
-      "photo":"",
+      "photo":"https://www.sideshow.com/product-asset/9032461/feature",
       "scores":[
           4,
           2,
